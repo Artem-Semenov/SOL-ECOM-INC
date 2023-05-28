062bfa9fb5bb249f80eee172ebc05199ac140cd5
Artem <artem.semenov.101@gmail.com>
2023-05-28 21:35:36 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -1,36 +1,7 @@
 'use strict'
-
-let slider = new Swiper('.page', {
-  wrapperClass: "page__wrapper",
-  slideClass: "page__screen",
-
-  direction: "vertical",
-
-  sliderPerView: "auto",
-
-  parallax: true,
-
-  keyboard: {
-    enabled: true,
-
-    onlyInViewPort: true,
-
-    pageUpDown: true,
-  },
-  mousewheel: {
-    sensitivity: 1,
-
-  },
-  watchOverflow: true,
-
-  speed: 800,
-
-  observerParents: true,
-  scrollbar: {
-    el: ".page__scroll",
-    dragClass: "page__drag-scroll",
-    draggable: true,
-  },
-
-  // simulateTouch: true
-})
+new fullpage('#fullpage', {
+	licenseKey: "gplv3-license",
+  keyboardScrolling: true,
+  // scrollBar: true
+  // scrollOverflow: true
+});
