831b9f212fe1044d667eaeada2553976a4b71502
Artem <artem.semenov.101@gmail.com>
2023-05-29 22:00:36 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -3,5 +3,22 @@ new fullpage('#fullpage', {
 	licenseKey: "gplv3-license",
   keyboardScrolling: true,
   // scrollBar: true
-  // scrollOverflow: true
+   scrollOverflow: false,
+  css3: true,
+  autoScrolling: true,
+  fitToSection: true,
+  fitToSectionDelay: 1000,
+  verticalCentered: true,
+  
+  onLeave: (origin, destination, direction, trigger) => {
+    const Nextnodes = destination.item.querySelectorAll('[data-aos]');
+    const previousNodes = origin.item.querySelectorAll('[data-aos]');
+    previousNodes.forEach(node => {
+      node.classList.remove('aos-animate')
+    });
+
+    Nextnodes.forEach(node => {
+      node.classList.add('aos-animate')
+    });
+  }
 });
