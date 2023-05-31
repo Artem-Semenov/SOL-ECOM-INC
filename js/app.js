912ccc106382b6af6561ba202da4d6784c60d2dc
Artem <artem.semenov.101@gmail.com>
2023-05-31 21:46:33 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -13,13 +13,16 @@ new fullpage("#fullpage", {
   onLeave: (origin, destination, direction, trigger) => {
     const Nextnodes = destination.item.querySelectorAll("[data-aos]");
     const previousNodes = origin.item.querySelectorAll("[data-aos]");
-    previousNodes.forEach((node) => {
-      node.classList.remove("aos-animate");
-    });
+    console.log(origin);
+    if (destination.index !== fullpage_api.test.Hn.length - 1) {
+      previousNodes.forEach((node) => {
+        node.classList.remove("aos-animate");
+      });
 
-    Nextnodes.forEach((node) => {
-      node.classList.add("aos-animate");
-    });
+      Nextnodes.forEach((node) => {
+        node.classList.add("aos-animate");
+      });
+    }
   },
 });
 
@@ -27,27 +30,49 @@ new fullpage("#fullpage", {
 
 const header = document.getElementById("header");
 const burgerBtn = document.querySelector(".header__burger-btn");
-const burgerBody = document.querySelector('.header__burger-body');
-const burgerContent = document.querySelector('.header__burger-body__content');
+const burgerBody = document.querySelector(".header__burger-body");
+const burgerContent = document.querySelector(".header__burger-body__content");
 if (burgerBtn) {
   burgerBtn.addEventListener("click", () => {
-  const menuOpen = header.classList.contains('_active')
+    const menuOpen = header.classList.contains("_active");
 
-  if (!menuOpen) {
-    burgerBtn.classList.add("_active");
-    header.classList.add("_active");
-    burgerBody.classList.add("_active");
-    setTimeout(() => {
-      burgerContent.classList.add("_active");
-    }, 400)
-  } else {
-    burgerBtn.classList.remove("_active");
-    header.classList.remove("_active");
-    burgerContent.classList.remove("_active");
-    setTimeout(() => {
-      burgerBody.classList.remove("_active");
-    }, 400)
-  }
-
+    if (!menuOpen) {
+      burgerBtn.classList.add("_active");
+      header.classList.add("_active");
+      burgerBody.classList.add("_active");
+      setTimeout(() => {
+        burgerContent.classList.add("_active");
+      }, 400);
+    } else {
+      burgerBtn.classList.remove("_active");
+      header.classList.remove("_active");
+      burgerContent.classList.remove("_active");
+      setTimeout(() => {
+        burgerBody.classList.remove("_active");
+      }, 400);
+    }
   });
 }
+
+//sub-menu
+
+const subMenuBody = document.querySelector(".header__burger-body_sub-list");
+const subMenuBtn = document.querySelector(".header__burger-body_arrow");
+
+const togglesubMenuOpen = () => {
+  subMenuBtn.classList.toggle("open");
+  subMenuBody.classList.toggle("open");
+};
+const addTogglingOnMobile = () => {
+  if (window.innerWidth < 1024) {
+    subMenuBtn.addEventListener("click", togglesubMenuOpen);
+  } else {
+    if (subMenuBtn.classList.contains("open")) {
+      togglesubMenuOpen();
+    }
+    subMenuBtn.removeEventListener("click", togglesubMenuOpen);
+  }
+};
+
+window.addEventListener("resize", addTogglingOnMobile);
+addTogglingOnMobile();
